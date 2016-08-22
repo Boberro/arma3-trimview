02ad844293fab1d3d0d2e095706dd95f0a5d50db
boberro <m.cyraniak@gmail.com>
2016-08-22 21:43:32 +0200

    removed userconfig

--- userconfig/bbrr_trimview/bbrr_trimview_config.hpp
+++
@@ -1,8 +0,0 @@
-// CARE NOT TO OMIT THE INCLUDED SEMI-COLONS AT END OF EACH LINE!
-// KEYBINDS SET IN GAME VIA: CONFIGURE > CONTROLS > CONFIGURE ADDONS
-
-// View distance that your normal setting will be trimmed to, if you press a button.
-BBRR_TRIMVIEW_SHORTENED_VIEW_DISTANCE = 200;
-
-// View distance that your normal setting will be lengthened to, if you press a button.
-BBRR_TRIMVIEW_LENGTHENED_VIEW_DISTANCE = 5000;
